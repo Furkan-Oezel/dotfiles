183b4b25820e6e53bed9390eb1c6e6b15ae3817e
Furkan Özel <work.kan314@gmail.com>
2024-03-26 11:03:32 +0100

    add fd-find to packet list

--- scripts_dir/install_packages.sh
+++ scripts_dir/install_packages.sh
@@ -3,6 +3,6 @@
 echo '<<<<--------------------------------------->>>>'
 echo 'installing packages'
 sudo dnf upgrade
-sudo dnf install neovim zsh kitty gnome-tweaks lsd gcc llvm g++ lolcat neofetch cowsay
+sudo dnf install neovim zsh kitty gnome-tweaks lsd gcc llvm g++ lolcat neofetch cowsay fd-find
 echo '<<<<--------------------------------------->>>>'
 echo 'OK'
